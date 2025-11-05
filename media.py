bb4aae4e36ac8ac728c21730c56d7d48cc868749
Hermag <jrhermesm@hotmail.com>
2025-11-05 10:28:16 -0300

    início do código de média

---
+++ media.py
@@ -0,0 +1,5 @@
+aluno=input('Digite o nome do aluno: ')
+nota1=float(input(f'Digite a primeira nota do aluno {aluno}: '))
+nota2=float(input(f'Digite a primeira nota do aluno {aluno}: '))
+media=(nota1+nota2)/2
+print(f'{media:.2f}')
