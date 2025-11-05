faaa85e7701f829fb42394a0d4cd84eb9bbd5506
Hermag <jrhermesm@hotmail.com>
2025-11-05 11:05:36 -0300

    adicionamos a verificação de status da média

--- media.py
+++ media.py
@@ -2,4 +2,12 @@ aluno=input('Digite o nome do aluno: ')
 nota1=float(input(f'Digite a primeira nota do aluno {aluno}: '))
 nota2=float(input(f'Digite a primeira nota do aluno {aluno}: '))
 media=(nota1+nota2)/2
-print(f'a média do {aluno} é {media:.2f}')
+situacao='A'
+if media>=7:
+    situacao='Aprovado'
+elif media >=4:
+    situacao="em Recuperação"
+else:
+    situacao='Reprovado' 
+
+print(f'a média do {aluno} é {media:.2f} e o seu status é de {situacao}')
