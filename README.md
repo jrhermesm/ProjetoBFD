c8ec66bc87733045a92f124b9e7b620d31f0c7cc
Hermag <jrhermesm@hotmail.com>
2025-11-05 10:58:26 -0300

    mudança do código de média

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ProjetoBFD
-# ProjetoBFD
--- media.py
+++ media.py
@@ -2,4 +2,4 @@ aluno=input('Digite o nome do aluno: ')
 nota1=float(input(f'Digite a primeira nota do aluno {aluno}: '))
 nota2=float(input(f'Digite a primeira nota do aluno {aluno}: '))
 media=(nota1+nota2)/2
-print(f'{media:.2f}')
+print(f'a média do {aluno} é {media:.2f}')
